8484660a66955745143d40f47f513662dc5ed236
Himanshu Kumar <himanshukumar802123@gmail.com>
2024-07-30 17:49:43 +0530

    Added JavaScript functionality to delete the list Item

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 const inputText = document.querySelector('#inputText');
 const btn = document.querySelector('.btn');
 const listItem = document.querySelector('.list-item');
+const err = document.querySelector('.err');
 
 btn.addEventListener('click', (e)=>{
     e.preventDefault();
     const text = inputText.value.trim();
     if(text !== ''){
-        const li = document.createElement('li');
+    const li = document.createElement('li');
        li.textContent = text;
        inputText.value = '';
        listItem.appendChild(li);
+        const deleteBtn = createButton('list-item li remove-item');
+        li.appendChild(deleteBtn);
 
     }else{
-        alert('Please add an Item')
+        err.style.color = 'orangeRed';
+        err.style.fontSize  = '14px'
+        err.textContent = 'Please enter a value';
+        setTimeout((e)=>{
+        err.textContent = '';
+         },2000)
     }
+    
 })
 function createButton(classes){
     const button = document.createElement('button');
     button.className = classes;
     const icon = createIcon('fa-solid fa-xmark');
     button.appendChild(icon);
+
+    button.addEventListener('click', ()=>{
+        button.parentElement.remove();
+    });
     return button;
 }
  function createIcon(classes){
@@ -28,4 +41,3 @@ function createButton(classes){
     return icon; 
  }
 
-    
